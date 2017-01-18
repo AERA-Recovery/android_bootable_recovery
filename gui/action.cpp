NamelessRom/android_bootable_recovery-twrp

gui: Actions: Toggle backlight on power key
Create GUIAction to handle KEY_POWER with a screen backlight toggle.

Change-Id: Iad0a7923b4a776e0336722db74d6fc46cd0107a2

gui/action.cpp
@@ -194,6 +194,7 @@ GUIAction::GUIAction(xml_node<>* node)
 		ADD_ACTION(checkpartitionlifetimewrites);
 		ADD_ACTION(mountsystemtoggle);
 		ADD_ACTION(setlanguage);
+		ADD_ACTION(setbacklight);
 
 		// remember actions that run in the caller thread
 		for (mapFunc::const_iterator it = mf.begin(); it != mf.end(); ++it)
@@ -1756,5 +1757,11 @@ int GUIAction::setlanguage(std::string arg __unused)
 	op_status = 0; // success
 
 	operation_end(op_status);
+	return 0;
+}
+
+int GUIAction::setbacklight(std::string arg __unused)
+{
+	blankTimer.toggleBlank();
 	return 0;
 }

AERA-Recovery/android_bootable_recovery

gui/action.cpp
@@ -198,6 +198,7 @@ GUIAction::GUIAction(xml_node<>* node)
 		ADD_ACTION(mountsystemtoggle);
 		ADD_ACTION(setlanguage);
 		ADD_ACTION(checkforapp);
+		ADD_ACTION(togglebacklight);
 
 		// remember actions that run in the caller thread
 		for (mapFunc::const_iterator it = mf.begin(); it != mf.end(); ++it)
@@ -1862,6 +1863,12 @@ int GUIAction::setlanguage(std::string arg __unused)
 	op_status = 0; // success
 
 	operation_end(op_status);
+	return 0;
+}
+
+int GUIAction::togglebacklight(std::string arg __unused)
+{
+	blankTimer.toggleBlank();
 	return 0;
 }
 