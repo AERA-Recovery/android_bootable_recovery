AERA-Recovery/android_bootable_recovery

commit a0966618f9bd753134a568d460a7c63adf42d8ab

Try to avoid spontaneous reboots to fastboot mode when using custom encryption

twrp-functions.cpp
@@ -279,6 +279,8 @@ bool i = Path_Exists("/tmp/orangefox.cfg");
 /* rerun startup if needed after decryption */
 bool TWFunc::Rerun_Startup(void)
 {
+   return false; // something is causing spontaneous reboots to fastboot, so just exit
+   
    if (OrangeFox_Startup_Executed > 0)
       return false;
 

commit f20ea033697fc97cf1324b331f4e3aeac9f5408b

Fix fancy decryption page * DARTHJABBA9 TRIGGERED * Originally introduced in f7c927e8 then broken in a0966618 Changes: * `data_decrypted` var is removed from c++ code. Now it will be set in xml * removed `used_custom_encryption` var because it does nothing * removed gui_loadCustomResources() because we reload theme in xml files

twrp-functions.cpp
@@ -334,7 +334,6 @@ bool TWFunc::Rerun_Startup(void)
    if (i == true || tprop == "1")
      return false;
 
-   DataManager::SetValue("data_decrypted", "1");
    // LOGINFO("OrangeFox: Reading settings file - again...\n");
    DataManager::ReadSettingsFile();
 

twrp.cpp
@@ -274,7 +274,6 @@ int main(int argc, char **argv)
 	LOGINFO("Backup of TWRP ramdisk done.\n");
 #endif
 
-	DataManager::SetValue("data_decrypted", "0");
 	// Offer to decrypt if the device is encrypted
 	if (DataManager::GetIntValue(TW_IS_ENCRYPTED) != 0) {
 		if (SkipDecryption) {
@@ -287,7 +286,6 @@ int main(int argc, char **argv)
 				  LOGINFO("- DEBUG: OrangeFox OTA: detected custom encryption\n");
 				  DataManager::SetValue("OTA_decrypted", "1");
 				  TWFunc::check_selinux_support();
-				  gui_loadCustomResources();
 			       } 
 			 }
 			else //
@@ -297,7 +295,6 @@ int main(int argc, char **argv)
 			LOGINFO("Is encrypted, do decrypt page first\n");			
 			//
 			LOGINFO("- DEBUG: OrangeFox: detected custom encryption\n");
-			DataManager::SetValue("used_custom_encryption", "1");
 			usleep(16);
 			//
 			if (gui_startPage("decrypt", 1, 1) != 0) {
@@ -305,7 +302,6 @@ int main(int argc, char **argv)
 			} else {
 				// Check for and load custom theme if present
 				TWFunc::check_selinux_support();
-				gui_loadCustomResources();
 			}
 		}
 	} else if (datamedia) {
@@ -413,7 +409,7 @@ int main(int argc, char **argv)
   // LOGINFO("OrangeFox: Reloading theme to apply generated theme on sdcard - again...\n");
   if (DataManager::GetStrValue("data_decrypted") == "1")
   {
-	//[f/d] Start UI using reapply_settings page
+	//[f/d] Start UI using reapply_settings page (executed on recovery startup)
   	DataManager::SetValue("of_reload_back", "main");
 	PageManager::RequestReload();
     gui_startPage("reapply_settings", 1, 0);