0bc0225fcf36afa348b01d3c0fcf6d04cb1affb4
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-07-11 18:22:28 +0800

    recovery: Add se_omapi
    * useful for decrypt strongbox device
    * usage example:
    TW_INCLUDE_OMAPI := true
    TW_OMAPI_UUID := 534552454144595f48414c5f55554944
    
    (cherry picked from commit 347c3eda6a5fb2535f7867fdf4a56ae94021cf74)

--- Android.mk
+++ Android.mk
@@ -351,7 +351,7 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
     TW_INCLUDE_CRYPTO_FBE := true
     LOCAL_CFLAGS += -DTW_INCLUDE_FBE
     LOCAL_SHARED_LIBRARIES += android.frameworks.stats@1.0 android.hardware.authsecret@1.0 \
-	android.security.authorization-ndk \
+        android.security.authorization-ndk \
         android.hardware.oemlock@1.0 libf2fs_sparseblock \
         libandroidicu.recovery \
         lib_android_keymaster_keymint_utils \
